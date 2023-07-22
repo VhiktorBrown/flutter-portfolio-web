cb8fa5566ff326523e10e280ff4822f3cb992b19
OWNER <bukkychukwujekwu@gmail.com>
2023-07-22 23:20:22 +0100

    set up web sdk. ran web app on chrome for first time

---
+++ lib/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: AppColors.primaryColor,
+    );
+  }
+
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+mixin AppColors {
+  static const Color primaryColor = Color(0xFF252733);
+  static const Color secondaryColor = Color(0XFF333645);
+
+  static const Color accentColor = Vx.purple400;
+}
